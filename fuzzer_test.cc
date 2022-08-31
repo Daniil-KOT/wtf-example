7245f02c8b1d38ecda99b4c9a4331ceaedd76a95
Daniil Dulov <ddulov@aladdin.ru>
2022-08-31 11:02:22 +0300

    fuzzer module code example

---
+++ fuzzer_test.cc
@@ -0,0 +1,51 @@
+// Axel '0vercl0k' Souchet - July 10 2021
+#include "backend.h"
+#include "targets.h"
+#include <fmt/format.h>
+#include <iostream>
+#include <iomanip>
+
+namespace fs = std::filesystem;
+
+namespace Test {
+
+constexpr bool LoggingOn = false;
+
+template <typename... Args_t>
+void DebugPrint(const char *Format, const Args_t &...args) {
+  if constexpr (LoggingOn) {
+    fmt::print("Test: ");
+    fmt::print(fmt::runtime(Format), args...);
+  }
+}
+
+bool InsertTestcase(const uint8_t *Buffer, const size_t BufferSize) {
+
+  if (!g_Backend->VirtWrite(Gva_t(g_Backend->Rax()), Buffer, BufferSize <= 10 ? BufferSize : 10 , true)) {
+    DebugPrint("Failed to write next testcase!");
+    return false;
+  }
+
+  return true;
+}
+
+bool Init(const Options_t &Opts, const CpuState_t &) {
+  
+  if (!g_Backend->SetBreakpoint(Gva_t(0x7ff7c5d06cf6), [](Backend_t *Backend) { 
+          DebugPrint("Reached function end\n");
+          Backend->Stop(Ok_t());
+      })) 
+  {
+    return false;
+  }
+
+  return true;
+}
+
+//
+// Register the target.
+//
+
+Target_t Test("test", Init, InsertTestcase);
+
+} // namespace Test
